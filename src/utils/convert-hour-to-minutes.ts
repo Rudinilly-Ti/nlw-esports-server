67ca7531afa388c1b2af54236536cc20088a1689
Rudinilly-ti <rudinilly-ti@outolook.com>
2022-09-18 22:13:53 -0300

    final project

---
+++ src/utils/convert-hour-to-minutes.ts
@@ -0,0 +1,6 @@
+export function convertHourToMinutes(hour: String) {
+  const [hours, minutes] = hour.split(':').map(Number)
+
+  const minutesAmount = (hours * 60) + minutes;
+  return minutesAmount
+}
